cce956779e8e01aef36f0f5861dcb6ddaef0818d
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-02-28 15:38:53 +0700

    fix GitHub runner saving CSV

--- default_cache_filename.js
+++ default_cache_filename.js
@@ -7,16 +7,15 @@ const pkg = require('./package.json')
 
 module.exports = (factory) => path.join(
   ...(process.platform === 'win32'
-      ? (env.LOCALAPPDATA || env.APPDATA)
+      ? env.LOCALAPPDATA || env.APPDATA
         ? [env.LOCALAPPDATA || env.APPDATA]
         : [home, 'AppData', 'Local']
       : process.platform === 'darwin'
         ? [home, 'Library', 'Caches']
-        : (env.XDG_CACHE_HOME && path.isAbsolute(env.XDG_CACHE_HOME))
+        : env.XDG_CACHE_HOME && path.isAbsolute(env.XDG_CACHE_HOME) && fs.existsSync(env.XDG_CACHE_HOME)
           ? [env.XDG_CACHE_HOME]
           : fs.existsSync(path.join(home, '.cache'))
             ? [home, '.cache']
-            : [os.tmpdir()]
-  ),
+            : [os.tmpdir()],
   `${pkg.name}_${factory.toLowerCase()}.csv`
 )
