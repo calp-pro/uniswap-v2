d4eb8dafedd4a62a362354b98b94fe7f4d2bbe2f
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-02-28 15:00:12 +0700

    fix GitHub runner

--- default_cache_filename.js
+++ default_cache_filename.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const env = process.env
+const fs = require('fs')
 const os = require('os')
 const home = os.homedir()
 const pkg = require('./package.json')
@@ -13,7 +14,9 @@ module.exports = (factory) => path.join(
         ? [home, 'Library', 'Caches']
         : (env.XDG_CACHE_HOME && path.isAbsolute(env.XDG_CACHE_HOME))
           ? [env.XDG_CACHE_HOME]
-          : [home, '.cache']
+          : fs.existsSync(path.join(home, '.cache'))
+            ? [home, '.cache']
+            : [os.tmpdir()]
   ),
   `${pkg.name}_${factory.toLowerCase()}.csv`
 )
