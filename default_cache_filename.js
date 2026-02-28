7b0df46cba87bfece7f066ecbec53f81cb590b7b
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-02-28 23:40:15 +0700

    fix default_cache_filename.js

--- default_cache_filename.js
+++ default_cache_filename.js
@@ -1,23 +1,24 @@
 const path = require('path')
-const env = process.env
+const {GITHUB_ACTIONS, XDG_CACHE_HOME, LOCALAPPDATA, APPDATA} = process.env
 const fs = require('fs')
 const os = require('os')
 const home = os.homedir()
 const pkg = require('./package.json')
 
-module.exports = (factory) => path.join(
-  ...(env.GITHUB_ACTIONS
-      ? ['.']
-      : process.platform === 'win32'
-      ? env.LOCALAPPDATA || env.APPDATA
-        ? [env.LOCALAPPDATA || env.APPDATA]
-        : [home, 'AppData', 'Local']
-      : process.platform === 'darwin'
-        ? [home, 'Library', 'Caches']
-        : env.XDG_CACHE_HOME && path.isAbsolute(env.XDG_CACHE_HOME) && fs.existsSync(env.XDG_CACHE_HOME)
-          ? [env.XDG_CACHE_HOME]
-          : fs.existsSync(path.join(home, '.cache'))
-            ? [home, '.cache']
-            : [os.tmpdir()],
-  `${pkg.name}_${factory.toLowerCase()}.csv`
+module.exports = factory => path.join(
+    ...(GITHUB_ACTIONS
+        ? ['.']
+        : process.platform == 'win32'
+            ? LOCALAPPDATA || APPDATA
+                ? [LOCALAPPDATA || APPDATA]
+                : [home, 'AppData', 'Local']
+            : process.platform == 'darwin'
+                ? [home, 'Library', 'Caches']
+                : XDG_CACHE_HOME && path.isAbsolute(XDG_CACHE_HOME) && fs.existsSync(XDG_CACHE_HOME)
+                    ? [XDG_CACHE_HOME]
+                    : fs.existsSync(path.join(home, '.cache'))
+                        ? [home, '.cache']
+                        : [os.tmpdir()]
+    ),
+    `${pkg.name}_${factory.toLowerCase()}.csv`
 )
